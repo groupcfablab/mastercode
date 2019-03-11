7a71e3738378f18d2036fbd4f5960761977e04d2
ateivis333 <46323237+ateivis333@users.noreply.github.com>
2019-03-11 14:41:34 +0000

    Neater setup

--- Code/ArduinoMotorControl/CarControl.ino
+++ Code/ArduinoMotorControl/CarControl.ino
@@ -6,12 +6,29 @@ controlsys motor_left, motor_right;
 
 void setup()
 {
-	const int directionpin_left = 5;
-	const int directionpin_right = 6;
-	const int tempbuttonpins_left[3] = { 14,15,16 };
-	const int tempbuttonpins_right[3] = { 14,15,16 };
-	motor_left.setup(openloop, 5, directionpin_left, tempbuttonpins_left, A5, int_0, 400, 100, 49, 150, 1000);
-	motor_right.setup(openloop, 5, directionpin_right, tempbuttonpins_right, A5, int_1, 400, 100, 49, 150, 1000);
+	const controllertype system_type = openloop;
+	const int pwmpin__left = 5;
+	const int pwmpin__right = 6;
+
+	const int directionpin_left = 52;
+	const int directionpin_right = 22;
+
+	const int buttonpins[3] = { 38,40,42 };
+	const int potentiometerpin = A0;
+
+	const ArduinoInterruptNames interruptpin_left = int_4;//int_4=D2
+	const ArduinoInterruptNames interruptpin_right = int_5;//int_5=D3
+
+	const int reading_rate_ms = 400;
+	const int pwm_update_rate_ms = 100;
+
+	const int magnets_on_circle = 49;
+	const int min_pwm_for_motor = 60;
+
+	const int max_rated_rpm = 2000;
+
+	motor_left.setup(system_type, pwmpin__left, directionpin_left, buttonpins, potentiometerpin, interruptpin_left, reading_rate_ms, pwm_update_rate_ms, magnets_on_circle, min_pwm_for_motor, max_rated_rpm);
+	motor_right.setup(system_type, pwmpin__right, directionpin_right, buttonpins, potentiometerpin, interruptpin_right, reading_rate_ms, pwm_update_rate_ms, magnets_on_circle, min_pwm_for_motor, max_rated_rpm);
 }
 
 void loop()
